e489aa08c7e73b523758d3be4f6dc77a86b9ecc3
Urmat Bekboev <bek.urmat@gmail.com>
2018-12-28 12:48:56 +0600

    Added Burger component

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
+import { INGREDIENTS } from "../Ingredient/Ingredient";
 import './Burger.css';
 
 class Burger extends Component {
-   render() {
-       return (
-           <div className="Burger">
-               <div className="BreadTop">
-                   <div className="Seeds1"> </div>
-                   <div className="Seeds2"> </div>
-               </div>
-               <div className="Salad"> </div>
-               <div className="Cheese"> </div>
-               <div className="Meat"> </div>
-               <div className="BreadBottom"> </div>
-           </div>
-       );
-   }
+    render() {
+        const ingredients = [];
+        let totalCost = 20;
+
+        this.props.data.forEach((item, index) => {
+            for (let i = 0; i < item.count; i++) {
+                totalCost += INGREDIENTS[index].price;
+                ingredients.push({name: item.name});
+            }
+        });
+
+        return (
+            <div className="Burger">
+                <div className="BreadTop">
+                    <div className="Seeds1"> </div>
+                    <div className="Seeds2"> </div>
+                </div>
+                {ingredients.map((item, index) => (
+                    <div className={item.name} key={index}> </div>
+                ))}
+                <div className="BreadBottom"> </div>
+                <p>Общая стоимость: {totalCost} сом</p>
+            </div>
+        );
+    }
 }
 
 export default Burger;
